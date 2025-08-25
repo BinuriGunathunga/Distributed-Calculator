a0e3e2f035b7a5c232c9866229d9e4663c41edf8
BinuriGunathunga <binurigunathunga@gmail.com>
2025-08-25 21:48:06 +0530

    first commit

---
+++ Distributed Calculator/CalculatorClient/Program.cs
@@ -0,0 +1,34 @@
+﻿// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+       
+//         int a = 15;
+//         int b = 70;
+//         int sum = a + b;
+
+//         Console.WriteLine($"Sum of {a} and {b} is: {sum}");
+//     }
+// }
+
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int sum = a + b;
+
+        Console.WriteLine($"Sum of {a} and {b} is: {sum}");
+    }
+}
